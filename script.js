af3b0a7b5e5e8cc188fbf7eed42b0b685c4d6531
19121A05T5 <86054844+19121A05T5@users.noreply.github.com>
2021-11-30 19:39:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let a=document.getElementById("amount").value;
+	let b=document.getElementById("myRange").value;
+	let c=document.getElementById("sel").value;
+	let d=(a*b*c)/100;
+	let e=parseInt(c)+2020;
+	document.getElementById("simple").innerHTML="If you deposit".concat(" ",a)+","+"<br>"+"at an interest rate of".concat(" ",b)+"%"+"."+"<br>"+"you will recieve an amount".concat(" ",d)+","+"<br>"+"in the year of".concat(" ",e);
+
 }
-        
